c5305ed6c983d723053ccdc3c6038eaf79429bf4
Goncalo Tojeiro <dev@tojeiro.me>
2023-06-06 22:23:22 +0100

    v1.0.0: eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,103 @@
+// This is a patch so that eslint will load the plugins as dependencies.
+require('@rushstack/eslint-patch/modern-module-resolution');
+
 module.exports = {
+  root: true,
+  extends: [
+    'airbnb',
+    'airbnb-base',
+    'prettier',
+    'plugin:import/recommended',
+  ],
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    sourceType: 'module',
+    requireConfigFile: false,
+    babelOptions: {
+    },
+    ecmaVersion: 2022,
+    ecmaFeatures: {
+   }
+  },
   env: {
     browser: true,
-    commonjs: true,
-    es2020: true,
-    "jest/globals": true,
-  },
-  extends: ["eslint:recommended", "airbnb/base", "plugin:prettier/recommended"],
-  parserOptions: {
-    ecmaVersion: 12,
+    node: true,
+    es2022: true,
   },
   rules: {
-    "no-console": "off",
+    'import/no-unresolved': [2, {commonjs: true, amd: true}],
+    'import/named': 2,
+    'import/namespace': 2,
+    'import/default': 2,
+    'import/export': 2,
+    'no-debugger': 0,
+    'no-use-before-define': 'off',
+    'import/no-cycle': 'off',
+    'no-alert': 0,
+    'no-await-in-loop': 0,
+    'no-plusplus': [2, { allowForLoopAfterthoughts: true }],
+    'no-return-assign': ['error', 'except-parens'],
+    'no-restricted-syntax': [
+      2,
+      'ForInStatement',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+    'no-unused-vars': [
+      1,
+      {
+        ignoreRestSiblings: true,
+        argsIgnorePattern: 'res|next|^err',
+      },
+    ],
+    'prefer-const': [
+      'error',
+      {
+        destructuring: 'all',
+      },
+    ],
+    'arrow-body-style': [2, 'as-needed'],
+    'no-unused-expressions': [
+      2,
+      {
+        allowTaggedTemplates: true,
+      },
+    ],
+    'no-param-reassign': [
+      2,
+      {
+        props: false,
+      },
+    ],
+    'no-console': 0,
+    'import/prefer-default-export': 0,
+    import: 0,
+    'func-names': 0,
+    'space-before-function-paren': 0,
+    'comma-dangle': 0,
+    'max-len': 0,
+    'import/extensions': 0,
+    'no-underscore-dangle': 0,
+    'consistent-return': 0,
+    quotes: [
+      2,
+      'single',
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: true,
+      },
+    ],
+    'prettier/prettier': [
+      'error',
+      {
+        trailingComma: 'es6',
+        singleQuote: true,
+        printWidth: 80,
+        // below line only for windows users facing CLRF and eslint/prettier error
+        // non windows users feel free to delete it
+        endOfLine: 'auto',
+      },
+    ],
   },
-  plugins: ["jest"],
+  plugins: ['prettier'],
 };
